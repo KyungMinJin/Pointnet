f5fe2f868c15e8076e00fe1cc5f8e2841409b247
KyungMinJin <KyungMinJin>
2021-03-18 14:56:19 +0900

    'fix_typo'

--- Pointnet/model.py
+++ Pointnet/model.py
@@ -25,26 +25,26 @@ class STN3d(nn.Module):  # spatial transformer network 이미지를 orthogonal�
         self.bn4 = nn.BatchNorm1d(512)
         self.bn5 = nn.BatchNorm1d(256)
 
-        def forward(self, x):
-            batchsize = x.size()[0]
-            x = F.relu(self.bn1(self.conv1(x)))
-            x = F.relu(self.bn2(self.conv2(x)))
-            x = F.relu(self.bn3(self.conv3(x)))
-            x = torch.max(x, 2, keepdim=True)[0]
-            x = x.view(-1, 1024)
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
 
-            x = F.relu(self.bn4(self.fc1(x)))
-            x = F.relu(self.bn5(self.fc2(x)))
-            x = self.fc3(x)
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
 
-            iden = Variable(
-                torch.from_numpy(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32))).view(1, 9).repeat(batchsize,
-                                                                                                              1)
-            if x.is_cuda:
-                iden = iden.cuda()
-            x = x + iden
-            x = x.view(-1, 3, 3)
-            return x
+        iden = Variable(
+            torch.from_numpy(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32))).view(1, 9).repeat(batchsize,
+                                                                                                          1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
+        x = x.view(-1, 3, 3)
+        return x
 
 
 class STNkd(nn.Module):  # feature transform 64 x 64
@@ -54,8 +54,8 @@ class STNkd(nn.Module):  # feature transform 64 x 64
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
         self.fc1 = nn.Linear(1024, 512)
-        self.fc1 = nn.Linear(512, 256)
-        self.fc1 = nn.Linear(256, k * k)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k * k)
         self.relu = nn.ReLU()
 
         self.bn1 = nn.BatchNorm1d(64)
